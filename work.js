8d5ddbe60f2fc61d1611b6a9f7b8ef539510299c
Aishah Isom <aishahisom@AdminisatorsMBP.lan>
2023-04-19 22:45:13 -0400

    first commit

---
+++ work.js
@@ -0,0 +1,11 @@
+var byTen = 200;
+var divisibleByTen = null;
+
+if (byTen % 10 === 0) {
+  //if the number is divisible by 10 and the remainders are 0 then console log will show true.
+  divisibleByTen = true;
+  console.log(divisibleByTen);
+} else {
+  divisibleByTen = false; // if the number is not divisible by ten the remainders will not be 0 the function will be false
+  console.log(divisibleByTen);
+}
